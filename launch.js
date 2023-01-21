375835a74cf3bf1187d8ac91820303ad39a5451e
gradek64 <gradek64@yahoo.com>
2023-01-21 13:19:57 +0000

    full screen added

--- launch.js
+++ launch.js
@@ -3,7 +3,9 @@ const { debug } = require("./debug");
 const path = require("path");
 
 debug("process.env.DEBUG set =>", process.env.DEBUG, "green");
-
+/* full list of window options on github
+https://github.com/electron/electron/blob/main/docs/api/browser-window.md
+*/
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -11,6 +13,7 @@ function createWindow() {
     x: 0,
     y: 0,
     title: "Heater water app",
+    fullscreen: process.env.DEBUG ? false : true,
     autoHideMenuBar: true,
     frame: process.env.DEBUG ? true : false,
     webPreferences: {
